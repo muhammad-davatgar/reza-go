f57f55c88aee22dac2f0cacebd10214a2b3cef8a
muhammad-davatgar <md2002.110@gmail.com>
2023-08-21 18:07:31 +0330

    diamond

--- arrays/diamond.go
+++ arrays/diamond.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	n := 5
+	n := 13
 
 	arr := make([]string, n)
 
@@ -11,12 +11,8 @@ func main() {
 		arr[i] = " "
 	}
 
-	mid := 5 / 2
+	mid := n / 2
 	arr[mid] = "*"
-
-	// arr2 := []string{"*", "*", "*", "*", "*"}
-	// DecreaseStars(&arr2)
-	// fmt.Println(arr2)
 
 	for i := 0; i < n*2-1; i++ {
 		for j := range arr {
@@ -49,20 +45,20 @@ func IncreaseStars(arrPointer *[]string) {
 
 func DecreaseStars(arrPointer *[]string) {
 	found := false
-
 	for i := range *arrPointer {
 		if (*arrPointer)[i] == "*" && !found {
 			(*arrPointer)[i] = " "
 			found = true
-
+			continue
 		}
 
 		if ((*arrPointer)[i] == " " || i == (len(*arrPointer)-1)) && found {
-			if i == (len(*arrPointer) - 1) {
+			if i == (len(*arrPointer)-1) && (*arrPointer)[i] == "*" {
 				(*arrPointer)[i] = " "
 			} else {
 				(*arrPointer)[i-1] = " "
 			}
+			found = false
 		}
 	}
 }
