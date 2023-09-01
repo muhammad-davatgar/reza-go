670ad1430d31d5fe126c1a11f3c77c905066a4ba
muhammad-davatgar <md2002.110@gmail.com>
2023-09-01 13:20:11 +0330

    interface intro

---
+++ intr/intr.go
@@ -0,0 +1,48 @@
+package intr
+
+import "fmt"
+
+func main() {
+	c := Cat{}
+	Pet(c)
+
+	t := Tiger{}
+
+	l := Lion{}
+
+	Pet(t)
+	Pet(l)
+
+}
+
+type Cat struct{}
+
+func (Cat) Meaw() {
+	fmt.Println("meaw")
+}
+
+type Tiger struct{}
+
+func (Tiger) Meaw() {
+	fmt.Println("Meaaaaaw")
+}
+
+type Lion struct{}
+
+func (Lion) Meaw() {
+	fmt.Println("roar")
+}
+
+func Pet(pishih GorbeSan) {
+	pishih.Meaw()
+}
+
+type GorbeSan interface {
+	Meaw()
+}
+
+type Dog struct{}
+
+func (Dog) Bark() {
+	fmt.Println("bark")
+}
